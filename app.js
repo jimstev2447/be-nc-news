441c457050bb72130e8cc7d9d64835d6ce76ec5a
James Stevenson <jamesstevenson@laurens-MacBook-Air.local>
2019-11-25 14:15:32 +0000

    added cors to enable cross platform requests

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+
 app.use(express.json());
+app.use(cors());
 apiRouter = require('./routes/api-router');
 const {
   psql400s,
